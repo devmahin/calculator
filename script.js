526d4f11a42732230bb4469148b0099b4ed57e07
devmahin <mahinhowlader123@gmail.com>
2023-11-24 22:58:16 +0600

    js completed

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const input = document.getElementById("input")
+const button = document.querySelectorAll("button")
+let string = "";
+button.forEach((values,index) => {
+    values.addEventListener("click", (val,i) => {
+
+        if(val.target.innerHTML == "="){
+            string = eval(string)    
+            document.getElementById("input").value = string;
+        }else if(val.target.innerHTML == "C"){
+            string = "";
+            document.getElementById("input").value = string;
+        }else{
+            string += val.target.innerHTML
+            document.getElementById("input").value = string;
+        }
+
+    })
+})
+
+
+// function deleted () {
+//     document.getElementById("input").value = "";
+
+// }
